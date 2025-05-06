934e8c1e7bf803da39110cf4b08ea92bc01f9d30
Mykola Shevhenko <jonhsmithwich@gmail.com>
2025-05-06 20:00:20 +0300

    Add 'in_list_view' and 'in_standard_filter' properties for term_work_execution in Pass Enterprise

--- pass_ova/pass_ova/doctype/pass_enterprise/pass_enterprise_list.js
+++ pass_ova/pass_ova/doctype/pass_enterprise/pass_enterprise_list.js
@@ -1,5 +1,15 @@
 frappe.listview_settings["Pass Enterprise"] = {
      hide_name_column: false, // hide the last column which shows the `name`
      hide_name_filter: false, // hide the default filter field for the name column
+     add_fields: ['term_work_execution'],
+     get_indicator(doc) {
+          // customize indicator color
+          let now = frappe.datetime.get_today();
+          if (doc.term_work_execution >= now) {
+               return [__("Дозволено"), "green", "term_work_execution,>=," + now];
+          } else {
+               return [__("Заборонено"), "darkgrey", "term_work_execution,<," + now];
+          }
+     },
 };
 

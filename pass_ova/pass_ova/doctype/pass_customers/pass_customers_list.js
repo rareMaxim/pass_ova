e19e447eec980e52dbb2e90878e7825abd98694b
Mykola Shevhenko <jonhsmithwich@gmail.com>
2025-05-06 19:56:25 +0300

    Add 'in_list_view' property and customize indicator for term_work_execution in Pass Customers

--- pass_ova/pass_ova/doctype/pass_customers/pass_customers_list.js
+++ pass_ova/pass_ova/doctype/pass_customers/pass_customers_list.js
@@ -1,5 +1,15 @@
 frappe.listview_settings["Pass Customers"] = {
     hide_name_column: true, // hide the last column which shows the `name`
     hide_name_filter: true, // hide the default filter field for the name column
+    add_fields: ['term_work_execution'],
+    get_indicator(doc) {
+        // customize indicator color
+        let now = frappe.datetime.get_today();
+        if (doc.term_work_execution >= now) {
+            return [__("Дозволено"), "green", "term_work_execution,>=," + now];
+        } else {
+            return [__("Заборонено"), "darkgrey", "term_work_execution,<," + now];
+        }
+    },
 };
 
